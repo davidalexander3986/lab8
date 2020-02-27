1c8c2accf13b86045a444176667ed7b2c7005726
davidalexander3986 <youremail@university.edu>
2020-02-27 00:52:16 -0800

    thirdPartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+    L.mapquest.key = 'OpadhoGdG3NDcHO5NdNbrHzVVyehKsUh';
+
+    // 'map' refers to a <div> element with the ID map
+    var map = L.mapquest.map('map', {
+      center: [37.7749, -122.4194],
+      layers: L.mapquest.tileLayer('map'),
+      zoom: 12
+    });
+
+    L.marker([32.878927, 117.235885]).addTo(map);
 }
